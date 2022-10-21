0bad1ed533712e017a5149c95705fd20be569605
Siddharth Chaberia <72343060+SiddharthChaberia@users.noreply.github.com>
2022-10-21 15:40:08 +0530

    Global Round 23 - A
    Solution to problem A of Global Round 23 which took place on Codeforces platform

---
+++ Global_Round23A.py
@@ -0,0 +1,9 @@
+for _ in range(int(input())):
+    a=[int(i) for i in input().split()]
+    b=[int(i) for i in input().split()]
+    if max(b)==1:
+        print("YES")
+    else:
+        print("NO")
+
+        
